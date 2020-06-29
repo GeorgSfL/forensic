d4e42d8cabfec6fbcf8682dc75b5aaa526b146a4
Georg Samhaber <georgsamhaber@gmx.at>
2020-06-29 16:43:49 +0200

    forensic v1.0

---
+++ forensic.py
@@ -0,0 +1,16 @@
+suspects = {"Eva": ["TGAAGGACCTTC", "AAAACCTCA", "TTAGCTATCGC", "TTGTGGTGGC", "AGGCCTCA"],
+            "Larisa": ["TGAAGGACCTTC", "AAAACCTCA", "GCCAGTGCCG", "AAGTAGTGAC", "AGGCCTCA"],
+            "Matej": ["TGCAGGAACTTC", "AAAACCTCA", "CCAGCAATCGC", "TTGTGGTGGC", "AGGCCTCA"],
+            "Miha": ["TGCAGGAACTTC", "AAAACCTCA", "GCCAGTGCCG", "GGGAGGTGGC", "GCCACGG"]}
+
+with open("dna.txt", "r") as dna_file:
+        dna = dna_file.read()
+
+for thief in suspects:
+    arrest = True
+    for x in suspects[thief]:
+        if x not in dna:
+            arrest = False
+            break
+        if thief is True:
+            print(thief)
